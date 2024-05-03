79a282bfb2d0c993e982e4fb487cce65f5c85e40
Petrando <petrandorichard@gmail.com>
2024-05-03 21:43:10 +0800

    fix: change titles

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -50,7 +50,7 @@ const AboutSection = (props) => {
       <h1 className="text-4xl md:text-6xl font-extrabold leading-snug mt-8 md:mt-0">
         Hi, I'm
         <br />
-        <span className="bg-white px-1 italic">Wawa Sensei</span>
+        <span className="bg-white px-1 italic">Your Name</span>
       </h1>
       <motion.p
         className="text-lg text-gray-600 mt-4"
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -27,9 +27,9 @@ export const LoadingScreen = (props) => {
             width: `${progress}%`,
           }}
         >
-          Wawa Sensei
+          Loading
         </div>
-        <div className="opacity-40">Wawa Sensei</div>
+        <div className="opacity-40">Loading</div>
       </div>
     </div>
   );
